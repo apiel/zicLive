12de73b79293268954fbaca311a268332cd10853
Alexandre Piel <Alexandre.Piel@zooplus.com>
2023-02-06 21:38:56 +0100

    synth

--- src/patches/zicSynth.ts
+++ src/patches/zicSynth.ts
@@ -33,6 +33,7 @@ const col = config.screen.col;
 const add = config.screen.col === 1 ? 3 : 1;
 const rowAddGraph = config.screen.col === 1 ? () => rowGetAndAdd(add) : () => rowGet();
 
+// TODO should there be on top a way to change of seq??
 export default function (patch: Patch, scrollY: number) {
     rowReset();
     if (
@@ -133,8 +134,8 @@ export default function (patch: Patch, scrollY: number) {
     );
     drawField(
         `Type`,
-        ` ${FilterNames[patch.floats[fId.filterMode]]}`,
-        rowGetAndAdd(1),
+        `${FilterNames[patch.floats[fId.filterMode]]}`,
+        rowNext(col), //rowGetAndAdd(1),
         {
             edit: (direction) => {
                 patch.setNumber(
@@ -149,262 +150,116 @@ export default function (patch: Patch, scrollY: number) {
         },
     );
 
-    // drawField(
-    //     `Duration`,
-    //     patch.floats[fId.Duration].toString(),
-    //     rowGetAndAdd(1),
-    //     {
-    //         edit: (direction) => {
-    //             patch.setNumber(fId.Duration, minmax(patch.floats[fId.Duration] + direction, 10, 5000));
-    //         },
-    //         steps: [1, 10],
-    //     },
-    //     {
-    //         col,
-    //         info: `ms (t)`,
-    //         scrollY,
-    //     },
-    // );
+    drawField(
+        `Attack`,
+        patch.floats[fId.envAttack].toString(),
+        rowGetAndAdd(1),
+        {
+            edit: (direction) => {
+                patch.setNumber(
+                    fId.envAttack,
+                    minmax(patch.floats[fId.envAttack] + direction, 0, 9900),
+                );
+            },
+            steps: [10, 100],
+        },
+        {
+            scrollY,
+            info: `ms`,
+        },
+    );
 
-    // drawEnvelope(
-    //     [
-    //         [0, 0],
-    //         [1, 0.01], // Force to have a very short ramp up to avoid clicks
-    //         [patch.floats[fId.envAmp1], patch.floats[fId.envAmp1Time]],
-    //         [patch.floats[fId.envAmp2], patch.floats[fId.envAmp2Time]],
-    //         [patch.floats[fId.envAmp3], patch.floats[fId.envAmp3Time]],
-    //         [0.0, 1.0],
-    //     ],
-    //     { row: rowAddGraph(), col, scrollY },
-    // );
-    // drawFieldDual(
-    //     `AmpMod1`,
-    //     Math.round(patch.floats[fId.envAmp1] * 100).toString(),
-    //     Math.round(patch.floats[fId.envAmp1Time] * 100).toString(),
-    //     rowGetAndAdd(1),
-    //     {
-    //         edit: (direction) => {
-    //             patch.setNumber(fId.envAmp1, minmax(patch.floats[fId.envAmp1] + direction, 0, 1));
-    //         },
-    //         steps: [0.01, 0.1],
-    //     },
-    //     {
-    //         edit: (direction) => {
-    //             patch.setNumber(
-    //                 fId.envAmp1Time,
-    //                 minmax(patch.floats[fId.envAmp1Time] + direction, 0, patch.floats[fId.envAmp2Time]),
-    //             );
-    //         },
-    //         steps: [0.01, 0.1],
-    //     },
-    //     { info: '%', info2: '%t', scrollY },
-    // );
-    // drawFieldDual(
-    //     `AmpMod2`,
-    //     Math.round(patch.floats[fId.envAmp2] * 100).toString(),
-    //     Math.round(patch.floats[fId.envAmp2Time] * 100).toString(),
-    //     rowGetAndAdd(1),
-    //     {
-    //         edit: (direction) => {
-    //             patch.setNumber(fId.envAmp2, minmax(patch.floats[fId.envAmp2] + direction, 0, 1));
-    //         },
-    //         steps: [0.01, 0.1],
-    //     },
-    //     {
-    //         edit: (direction) => {
-    //             patch.setNumber(
-    //                 fId.envAmp2Time,
-    //                 minmax(
-    //                     patch.floats[fId.envAmp2Time] + direction,
-    //                     patch.floats[fId.envAmp1Time],
-    //                     patch.floats[fId.envAmp3Time],
-    //                 ),
-    //             );
-    //         },
-    //         steps: [0.01, 0.1],
-    //     },
-    //     { info: '%', info2: '%t', scrollY },
-    // );
-    // drawFieldDual(
-    //     `AmpMod3`,
-    //     Math.round(patch.floats[fId.envAmp3] * 100).toString(),
-    //     Math.round(patch.floats[fId.envAmp3Time] * 100).toString(),
-    //     rowGetAndAdd(1),
-    //     {
-    //         edit: (direction) => {
-    //             patch.setNumber(fId.envAmp3, minmax(patch.floats[fId.envAmp3] + direction, 0, 1));
-    //         },
-    //         steps: [0.01, 0.1],
-    //     },
-    //     {
-    //         edit: (direction) => {
-    //             patch.setNumber(
-    //                 fId.envAmp3Time,
-    //                 minmax(patch.floats[fId.envAmp3Time] + direction, patch.floats[fId.envAmp2Time], 1),
-    //             );
-    //         },
-    //         steps: [0.01, 0.1],
-    //     },
-    //     { info: '%', info2: '%t', scrollY },
-    // );
+    drawField(
+        `Decay`,
+        patch.floats[fId.envDecay].toString(),
+        rowGetAndAdd(1),
+        {
+            edit: (direction) => {
+                patch.setNumber(
+                    fId.envDecay,
+                    minmax(patch.floats[fId.envAttack] + direction, 0, 9900),
+                );
+            },
+            steps: [10, 100],
+        },
+        {
+            scrollY,
+            info: `ms`,
+        },
+    );
 
-    // drawEnvelope(
-    //     [
-    //         [1.0, 0.0],
-    //         [patch.floats[fId.envFreq1], patch.floats[fId.envFreq1Time]],
-    //         [patch.floats[fId.envFreq2], patch.floats[fId.envFreq2Time]],
-    //         [patch.floats[fId.envFreq3], patch.floats[fId.envFreq3Time]],
-    //         [0.0, 1.0],
-    //     ],
-    //     { row: rowAddGraph(), col, scrollY },
-    // );
-    // drawFieldDual(
-    //     `FrqMod1`,
-    //     Math.round(patch.floats[fId.envFreq1] * 100).toString(),
-    //     Math.round(patch.floats[fId.envFreq1Time] * 100).toString(),
-    //     rowGetAndAdd(1),
-    //     {
-    //         edit: (direction) => {
-    //             patch.setNumber(fId.envFreq1, minmax(patch.floats[fId.envFreq1] + direction, 0, 1));
-    //         },
-    //         steps: [0.01, 0.1],
-    //     },
-    //     {
-    //         edit: (direction) => {
-    //             patch.setNumber(
-    //                 fId.envFreq1Time,
-    //                 minmax(patch.floats[fId.envFreq1Time] + direction, 0, patch.floats[fId.envFreq2Time]),
-    //             );
-    //         },
-    //         steps: [0.01, 0.1],
-    //     },
-    //     { info: '%', info2: '%t', scrollY },
-    // );
-    // drawFieldDual(
-    //     `FrqMod2`,
-    //     Math.round(patch.floats[fId.envFreq2] * 100).toString(),
-    //     Math.round(patch.floats[fId.envFreq2Time] * 100).toString(),
-    //     rowGetAndAdd(1),
-    //     {
-    //         edit: (direction) => {
-    //             patch.setNumber(fId.envFreq2, minmax(patch.floats[fId.envFreq2] + direction, 0, 1));
-    //         },
-    //         steps: [0.01, 0.1],
-    //     },
-    //     {
-    //         edit: (direction) => {
-    //             patch.setNumber(
-    //                 fId.envFreq2Time,
-    //                 minmax(
-    //                     patch.floats[fId.envFreq2Time] + direction,
-    //                     patch.floats[fId.envFreq1Time],
-    //                     patch.floats[fId.envFreq3Time],
-    //                 ),
-    //             );
-    //         },
-    //         steps: [0.01, 0.1],
-    //     },
-    //     { info: '%', info2: '%t', scrollY },
-    // );
-    // drawFieldDual(
-    //     `FrqMod3`,
-    //     Math.round(patch.floats[fId.envFreq3] * 100).toString(),
-    //     Math.round(patch.floats[fId.envFreq3Time] * 100).toString(),
-    //     rowGetAndAdd(1),
-    //     {
-    //         edit: (direction) => {
-    //             patch.setNumber(fId.envFreq3, minmax(patch.floats[fId.envFreq3] + direction, 0, 1));
-    //         },
-    //         steps: [0.01, 0.1],
-    //     },
-    //     {
-    //         edit: (direction) => {
-    //             patch.setNumber(
-    //                 fId.envFreq3Time,
-    //                 minmax(patch.floats[fId.envFreq3Time] + direction, patch.floats[fId.envFreq2Time], 1),
-    //             );
-    //         },
-    //         steps: [0.01, 0.1],
-    //     },
-    //     { info: '%', info2: '%t', scrollY },
-    // );
+    drawField(
+        `Sustain`,
+        Math.round(patch.floats[fId.envSustain] * 100).toString(),
+        rowGetAndAdd(1),
+        {
+            edit: (direction) => {
+                patch.setNumber(fId.envSustain, minmax(patch.floats[fId.envSustain] + direction, 0, 1));
+            },
+            steps: [0.01, 0.1],
+        },
+        {
+            scrollY,
+            info: `%`,
+        },
+    );
 
-    // drawFieldDual(
-    //     `Distortion`,
-    //     patch.floats[fId.distortion].toString(),
-    //     patch.floats[fId.distortionRange].toString(),
-    //     rowGetAndAdd(1),
-    //     {
-    //         edit: (direction) => {
-    //             patch.setNumber(fId.distortion, minmax(patch.floats[fId.distortion] + direction, 0, 100));
-    //         },
-    //         steps: [1, 10],
-    //     },
-    //     {
-    //         edit: (direction) => {
-    //             patch.setNumber(fId.distortionRange, minmax(patch.floats[fId.distortionRange] + direction, 10, 120));
-    //         },
-    //         steps: [1, 10],
-    //     },
-    //     { scrollY },
-    // );
+    drawField(
+        `Release`,
+        patch.floats[fId.envRelease].toString(),
+        rowGetAndAdd(1),
+        {
+            edit: (direction) => {
+                patch.setNumber(
+                    fId.envRelease,
+                    minmax(patch.floats[fId.envAttack] + direction, 0, 9900),
+                );
+            },
+            steps: [10, 100],
+        },
+        {
+            scrollY,
+            info: `ms`,
+        },
+    );
 
-    // drawFieldDual(
-    //     ``,
-    //     `Reload`,
-    //     `Save`,
-    //     rowNext(1),
-    //     {
-    //         edit: withInfo('Loaded', () => loadPatchId('kick23', patch.id)),
-    //     },
-    //     {
-    //         edit: withSuccess('Saved', () => savePatch('kick23', patch.id)),
-    //     },
-    //     { scrollY },
-    // );
+    drawFieldDual(
+        `Env. Osc.`,
+        ` ${Math.round(patch.floats[fId.envModPitch] * 100)}`,
+        ` ${Math.round(patch.floats[fId.envModAmplitude] * 100)}`,
+        rowNext(1),
+        {
+            edit: (direction) => {
+                patch.setNumber(fId.envModPitch, minmax(patch.floats[fId.envModPitch] + direction, 0, 1));
+            },
+            steps: [0.01, 0.05],
+        },
+        {
+            edit: (direction) => {
+                patch.setNumber(fId.envModAmplitude, minmax(patch.floats[fId.envModAmplitude] + direction, 0, 1));
+            },
+            steps: [0.01, 0.05],
+        },
+        { info: '%frq', info2: '%amp', scrollY },
+    );
 
-    // drawField(
-    //     `Save as`,
-    //     saveAs,
-    //     rowGetAndAdd(1),
-    //     {
-    //         edit: () => {
-    //             savePatchAs('kick23', patch, saveAs);
-    //         },
-    //     },
-    //     {
-    //         col,
-    //         scrollY,
-    //     },
-    // );
-
-    // // drawFieldDual(
-    // //     ``,
-    // //     `Delete`,
-    // //     `Rename`, // ?
-    // //     row,
-    // //     {
-    // //         // TODO #4 patch delete. If patch used, they should be replaced with default patch
-    // //         edit: () => console.log('Delete to be implemented...'),
-    // //     },
-    // //     {
-    // //         // edit: () => savePatch('kick23', patch.id),
-    // //     },
-    // //     { scrollY },
-    // // );
-
-    // drawKeyboard(
-    //     (char) => {
-    //         if (char === 'DEL') {
-    //             saveAs = saveAs.slice(0, -1);
-    //         } else if (char === 'DONE') {
-    //             savePatchAs('kick23', patch, saveAs);
-    //         } else {
-    //             if (saveAs.length < 10) {
-    //                 saveAs += char;
-    //             }
-    //         }
-    //     },
-    //     { row: rowNext(1), col, scrollY, done: 'SAVE' },
-    // );
+    drawFieldDual(
+        `Env. Filter`,
+        ` ${Math.round(patch.floats[fId.envModCutoff] * 100)}`,
+        ` ${Math.round(patch.floats[fId.envModResonance] * 100)}`,
+        rowNext(col),
+        {
+            edit: (direction) => {
+                patch.setNumber(fId.envModCutoff, minmax(patch.floats[fId.envModCutoff] + direction, 0, 1));
+            },
+            steps: [0.01, 0.05],
+        },
+        {
+            edit: (direction) => {
+                patch.setNumber(fId.envModResonance, minmax(patch.floats[fId.envModResonance] + direction, 0, 1));
+            },
+            steps: [0.01, 0.05],
+        },
+        { info: '%frq', info2: '%res', scrollY, col },
+    );
 }

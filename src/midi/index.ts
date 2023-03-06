18e8faa966f6000cbc7cef8b702b488211d7c5cc
Alexandre Piel <Alexandre.Piel@zooplus.com>
2023-03-06 21:53:17 +0100

    midi navigation

--- src/midi/index.ts
+++ src/midi/index.ts
@@ -1,9 +1,14 @@
 import { getMidiDevices, MidiError, MidiMessage, setMidiCallback, subscribeMidiInput } from 'zic_node';
 import { Events, render } from 'zic_node_ui';
 import { drawError, renderMessage } from '../draw/drawMessage';
-import { KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_UP } from '../events';
+import { KEY_DOWN, KEY_EDIT, KEY_LEFT, KEY_MENU, KEY_RIGHT, KEY_UP } from '../events';
 import { viewEventHandler } from '../view';
 import { akaiApcKey25 } from './akaiApcKey25';
+
+enum MIDI_TYPE {
+    KEY_PRESSED = 144,
+    KEY_RELEASED = 128,
+}
 
 const events: Events = {
     keysDown: [],
@@ -14,48 +19,54 @@ const midiDevices = getMidiDevices();
 const midiInputController = midiDevices.input.find((input) => input.name.includes('APC Key 25 mk2 C'));
 const midiInputKeyboard = midiDevices.input.find((input) => input.name.includes('APC Key 25 mk2 K'));
 
-setMidiCallback(async (data) => {
-    if ((data as MidiError).error) {
-        console.error('midi error', data);
-        return;
-    }
-    const { port, message } = data as MidiMessage;
+async function basicUiEvent({ port, message: [type, padKey] }: MidiMessage) {
     // clear keysUp but not keysDown
     events.keysUp = [];
-    if (message[0] === 144) {
+    if (type === MIDI_TYPE.KEY_PRESSED) {
         // pressed
         if (port === midiInputController?.port) {
-            if (message[1] === akaiApcKey25.pad.up) {
+            if (padKey === akaiApcKey25.pad.up) {
                 events.keysDown!.push(KEY_UP);
-            } else if (message[1] === akaiApcKey25.pad.down) {
+            } else if (padKey === akaiApcKey25.pad.down) {
                 events.keysDown!.push(KEY_DOWN);
-            } else if (message[1] === akaiApcKey25.pad.left) {
+            } else if (padKey === akaiApcKey25.pad.left) {
                 events.keysDown!.push(KEY_LEFT);
-            } else if (message[1] === akaiApcKey25.pad.right) {
+            } else if (padKey === akaiApcKey25.pad.right) {
                 events.keysDown!.push(KEY_RIGHT);
+            } else if (padKey === akaiApcKey25.pad.volume) {
+                events.keysDown!.push(KEY_MENU);
+            } else if (padKey === akaiApcKey25.pad.pan) {
+                events.keysDown!.push(KEY_EDIT);
             }
         }
-    } else if (message[0] === 128) {
+    } else if (type === MIDI_TYPE.KEY_RELEASED) {
         // released
         if (port === midiInputController?.port) {
-            if (message[1] === akaiApcKey25.pad.up) {
+            if (padKey === akaiApcKey25.pad.up) {
                 events.keysDown = events.keysDown!.filter((key) => key !== KEY_UP);
                 events.keysUp.push(KEY_UP);
-            } else if (message[1] === akaiApcKey25.pad.down) {
+            } else if (padKey === akaiApcKey25.pad.down) {
                 events.keysDown = events.keysDown!.filter((key) => key !== KEY_DOWN);
                 events.keysUp.push(KEY_DOWN);
-            } else if (message[1] === akaiApcKey25.pad.left) {
+            } else if (padKey === akaiApcKey25.pad.left) {
                 events.keysDown = events.keysDown!.filter((key) => key !== KEY_LEFT);
                 events.keysUp.push(KEY_LEFT);
-            } else if (message[1] === akaiApcKey25.pad.right) {
+            } else if (padKey === akaiApcKey25.pad.right) {
                 events.keysDown = events.keysDown!.filter((key) => key !== KEY_RIGHT);
                 events.keysUp.push(KEY_RIGHT);
+            } else if (padKey === akaiApcKey25.pad.volume) {
+                events.keysDown = events.keysDown!.filter((key) => key !== KEY_MENU);
+                events.keysUp.push(KEY_MENU);
+            } else if (padKey === akaiApcKey25.pad.pan) {
+                events.keysDown = events.keysDown!.filter((key) => key !== KEY_EDIT);
+                events.keysUp.push(KEY_EDIT);
             }
         }
     }
 
+    const isUiEvent = events.keysDown?.length || events.keysUp.length;
     try {
-        if (events.keysDown?.length || events.keysUp.length) {
+        if (isUiEvent) {
             if (await viewEventHandler(events)) {
                 render();
             }
@@ -65,6 +76,18 @@ setMidiCallback(async (data) => {
         drawError((error as any).message);
         renderMessage();
         render();
+    }
+
+    return isUiEvent;
+}
+
+setMidiCallback(async (data) => {
+    if ((data as MidiError).error) {
+        console.error('midi error', data);
+        return;
+    }
+    if (await basicUiEvent(data as MidiMessage)) {
+        return;
     }
 });
 

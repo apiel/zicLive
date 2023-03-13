4f3cd0c3a74779239052545cfe504bbda72cb23a
Alexandre Piel <Alexandre.Piel@zooplus.com>
2023-03-13 21:22:33 +0100

    scp

--- scp.sh
+++ scp.sh
@@ -3,8 +3,10 @@
 echo "Build"
 npm run build
 
-echo "Clean dist folder"
-sshpass -f ./.sshpassword ssh pi@192.168.1.106 'rm -rf Music/zicLive/dist'
+# TODO dont clean folder and just copy modified files
+
+echo "Kill nodejs && Clean dist folder"
+sshpass -f ./.sshpassword ssh pi@192.168.1.106 'killall node && rm -rf Music/zicLive/dist'
 
 echo "Copy dist folder"
 sshpass -f ./.sshpassword scp -r dist pi@192.168.1.106:Music/zicLive

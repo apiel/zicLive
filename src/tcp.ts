dc202a0aaeac7e57a62628a247b152e2ab382806
Alexandre Piel <Alexandre.Piel@zooplus.com>
2023-03-14 21:10:36 +0100

    make the midi controller client instead of server

--- src/tcp.ts
+++ src/tcp.ts
@@ -5,17 +5,30 @@ import { handleMidi } from './midi';
 const port = 2323;
 
 let server: Server;
-const connections: Socket[] = [];
 
 export function startServer() {
     server = createServer((connection) => {
         console.log('client connected');
-        connections.push(connection);
 
         connection.on('end', () => {
             console.log('client disconnected');
-            connections.splice(connections.indexOf(connection), 1);
         });
+
+        connection.on('data', (buffer) => {
+            try {
+                const lines = buffer.toString().split('\n');
+                for (const line of lines) {
+                    if (line) {
+                        const data = JSON.parse(line);
+                        handleMidi(data);
+                        // console.log(data);
+                    }
+                }
+            } catch (error) {
+                console.error('TCP error:', error, buffer.toString());
+            }
+        });
+
         connection.pipe(connection);
     });
 
@@ -24,11 +37,15 @@ export function startServer() {
     });
 }
 
+let client: Socket | undefined;
+
 export function sendTcpMidi(data: MidiMessage) {
-    if (server) {
-        const jsonData = JSON.stringify(data);
-        for (const connection of connections) {
-            connection.write(jsonData + '\n');
+    if (client) {
+        try {
+            const jsonData = JSON.stringify(data);
+            client.write(jsonData + '\n');   
+        } catch (error) {
+            console.error('SendTcpMidi error:', error);
         }
     }
 }
@@ -36,7 +53,7 @@ export function sendTcpMidi(data: MidiMessage) {
 let retry = 0;
 export function startClient(host: string) {
     console.log('connecting to server...');
-    const client = connect({ port, host }, () => {
+    client = connect({ port, host }, () => {
         console.log('connected to server!');
     });
 
@@ -49,22 +66,8 @@ export function startClient(host: string) {
         }
     });
 
-    client.on('data', function (buffer) {
-        try {
-            const lines = buffer.toString().split('\n');
-            for (const line of lines) {
-                if (line) {
-                    const data = JSON.parse(line);
-                    handleMidi(data);
-                    // console.log(data);
-                }
-            }
-        } catch (error) {
-            console.error('TCP error:', error, buffer.toString());
-        }
-    });
-
     client.on('end', function () {
         console.log('disconnected from server');
+        client = undefined;
     });
 }
